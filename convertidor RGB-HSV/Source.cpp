85e20dbe952e7953f2e5a6865f571e03703b2748
disofenix117 <disofenix117@hotmail.com>
2018-10-18 15:12:17 -0500

    HSV 2 RGB lo intente :(

--- convertidor RGB-HSV/Source.cpp
+++ convertidor RGB-HSV/Source.cpp
@@ -59,9 +59,10 @@ color RGB2HSV(float R, float G, float B)
 color HSV2RGB(float H, float S, float V)
 {
 	//variables locales
-	int R, G, B, H[360],C,X,m,V[1];
+	int R, G, B,C,X,m;
+
 	C = V * S;
-	X = C * (1 - abs((H / 60)/  2 - 1));
+	X = C * (1 - abs((H / 60) - 1));
 	m = V - C;
 	if (H >= 0 && H <= 60)
 	{
@@ -71,10 +72,40 @@ color HSV2RGB(float H, float S, float V)
 	}
 	else if (H >= 60 && H <= 120)
 	{
-		R = C;
-		G = X;
+		R = X;
+		G = C;
 		B = 0;
 	}
+	else if (H >= 120 && H <= 180)
+	{
+		R = 0;
+		G = C;
+		B = X;
+	}
+	else if (H >= 180 && H <= 240)
+	{
+		R = 0;
+		G = X;
+		B = C;
+	}
+	else if (H >= 240 && H <= 300)
+	{
+		R = X;
+		G = 0;
+		B = C;
+	}
+	else if (H >= 300 && H <= 360)
+	{
+		R = C;
+		G = 0;
+		B = X;
+	}
+
+	Color.c1 = (R + m) * 255;
+	Color.c2 = (G + m) * 255;
+	Color.c3 = (B + m) * 255;
+	return (Color);
+
 }
 int main()
 {
@@ -87,33 +118,13 @@ int main()
 		cout << ". : : C O N V E R T I D O R   D E   C O L O R   R G B - H S V : : . " << endl << endl;
 		cout << "Que quiere convertir?" << endl << endl;
 
-		cout << "1) H S V - R G B." << endl;
-		cout << "2) R G B - H S V." << endl;
+		//cout << "1) H S V - R G B." << endl;
+		cout << "1) R G B - H S V." << endl;
 		cout << "0) Salir." << endl;
 		cin >> opc;
 		switch (opc)
 		{
 		case 1:
-			system("cls");
-			{
-				cout << "Ingrese el valor de H:";
-				cin >> H;
-				cout << "Ingrese el valor de S:";
-				cin >> S;
-				cout << "Ingrese el valor de V:";
-				cin >> V;
-				if (H >= 0 && H < 360 && S >= 0 && S <= 1 && V >= 0 && V <= 1)
-				{
-					C = V * S;
-					//X = C * (1 - (abs( H / (pi / 3) % 2 - 1)));
-					M = V - C;
-
-
-
-
-				}
-			}
-		case 2:
 			system("cls");
 			{
 				cout << "Ingrese el valor de R:";
@@ -126,11 +137,34 @@ int main()
 				H = Color.c1;
 				S = Color.c2;
 				V = Color.c3;
-												
-				cout << "EL VALOR DE COLOR PARA HSV ES:"<<endl;
-				cout << "H:" << H << "\t" << "S:" << S << "\t" << "V:" << V << "\t" << endl;
-			
+
+				cout << "EL VALOR DE COLOR PARA H-S-V ES:" << endl;
+				cout << "H:" << H << "°" << "\t" << "S:" << S << "%" << "\t" << "V:" << V << "%" << "\t" << endl;
+				break;
+
+				
 			}
+		case 2:
+			system("cls");
+			{
+				cout << "Ingrese el valor de H:";
+				cin >> H;
+				cout << "Ingrese el valor de S:";
+				cin >> S;
+				cout << "Ingrese el valor de V:";
+				cin >> V;
+				HSV2RGB(H, S, V);
+				R = Color.c1;
+				G = Color.c2;
+				B = Color.c3;
+				cout << "EL VALOR DE COLOR PARA R-G-B ES:" << endl;
+				cout << "R: " << R << "\t" << "G: " << G << "\t" << "B: " << B << "\t" << endl;
+				break;
+			}
+		case 0:
+		{
+			index = 0;
+		}
 		default:
 			break;
 		}
